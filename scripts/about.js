492d27f4f303aa68eedeefbdc4009eca4250886e
Mukhopadhyay <praneshmukherjee7@gmail.com>
2020-02-04 23:38:44 +0530

    idk at this point

--- scripts/about.js
+++ scripts/about.js
@@ -1,43 +1,43 @@
-// (function (window, undefined) {
-//     var
-//         doc = window.document,
-//         defualtStyle = "display:inline-block;",
-//         Textile = function (n) {
-//             var
-//                 wordDiv,
-//                 charDiv;
-//             this.words = [];
-//             this.chars = [];
-//             this.contentClone = n.innerHTML;
-//             this.n = n;
-//             n.innerHTML = '';
-//             var wordsArr = this.contentClone.split(' ');
-//             for (var i = 0, wordslen = wordsArr.length; i < wordslen;) {
-//                 wordDiv = doc.createElement('div');
-//                 wordDiv.style.cssText += defualtStyle;
-//                 var word = wordsArr[i++],
-//                     charsArr = word.split('');
-//                 for (var j = 0, charslen = charsArr.length; j < charslen;) {
-//                     charDiv = doc.createElement('div');
-//                     charDiv.style.cssText += defualtStyle;
-//                     charDiv.textContent = charsArr[j++];
-//                     wordDiv.appendChild(charDiv);
-//                     this.chars.push(charDiv);
-//                 }
-//                 n.appendChild(wordDiv);
-//                 this.words.push(wordDiv);
-//                 if (i !== wordslen) {
-//                     wordDiv.insertAdjacentHTML('afterEnd', ' ');
-//                 }
-//             }
-//         }
-//     Textile.prototype.revert = function () {
-//         this.n.innerHTML = this.contentClone;
-//     }
-//     if (typeof window === "object" && typeof window.document === "object") {
-//         window.Textile = window.T = Textile;
-//     }
-// })(window);
+(function (window, undefined) {
+    var
+        doc = window.document,
+        defualtStyle = "display:inline-block;",
+        Textile = function (n) {
+            var
+                wordDiv,
+                charDiv;
+            this.words = [];
+            this.chars = [];
+            this.contentClone = n.innerHTML;
+            this.n = n;
+            n.innerHTML = '';
+            var wordsArr = this.contentClone.split(' ');
+            for (var i = 0, wordslen = wordsArr.length; i < wordslen;) {
+                wordDiv = doc.createElement('div');
+                wordDiv.style.cssText += defualtStyle;
+                var word = wordsArr[i++],
+                    charsArr = word.split('');
+                for (var j = 0, charslen = charsArr.length; j < charslen;) {
+                    charDiv = doc.createElement('div');
+                    charDiv.style.cssText += defualtStyle;
+                    charDiv.textContent = charsArr[j++];
+                    wordDiv.appendChild(charDiv);
+                    this.chars.push(charDiv);
+                }
+                n.appendChild(wordDiv);
+                this.words.push(wordDiv);
+                if (i !== wordslen) {
+                    wordDiv.insertAdjacentHTML('afterEnd', ' ');
+                }
+            }
+        }
+    Textile.prototype.revert = function () {
+        this.n.innerHTML = this.contentClone;
+    }
+    if (typeof window === "object" && typeof window.document === "object") {
+        window.Textile = window.T = Textile;
+    }
+})(window);
 $(document).ready(function () {
     let underlineAppear = ele => {
         ele.children[0].children[1].style.transform = "translateX( 0% )"
@@ -77,22 +77,22 @@ $(document).ready(function () {
         }
     });
 
-    // var node = document.getElementById('animatePara');
-    // var text = new T(node);
+    var node = document.getElementById('animatePara');
+    var text = new T(node);
 
-    // function random(min, max) {
-    //     return (Math.random() * (max - min)) + min;
-    // }
+    function random(min, max) {
+        return (Math.random() * (max - min)) + min;
+    }
 
-    // text.chars.map(function (v, i) {
-    //     TweenMax.from(v, 1, {
-    //         opacity: 0,
-    //         x: random(-500, 500),
-    //         y: random(-500, 500),
-    //         z: random(-500, 500),
-    //         scale: .1,
-    //         delay: i * .02,
-    //         yoyo: true
-    //     });
-    // });
+    text.chars.map(function (v, i) {
+        TweenMax.from(v, 1, {
+            opacity: 0,
+            x: random(-500, 500),
+            y: random(-500, 500),
+            z: random(-500, 500),
+            scale: .1,
+            delay: i * .02,
+            yoyo: true
+        });
+    });
 })
