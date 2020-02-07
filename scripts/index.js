862b2f8102c54fb7d76bfbbad306b96cf32d5a23
Mukhopadhyay <praneshmukherjee7@gmail.com>
2020-02-07 11:10:15 +0530

    Hugeeee changes

--- scripts/index.js
+++ scripts/index.js
@@ -29,18 +29,18 @@ $(document).ready(function () {
         afterLoad: function (anchorLink, index) { },
         afterRender: function () { },
     });
-    $('.logo-container').tilt({
-        maxTilt: 60,
-        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
-        easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-        scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
-        speed: 300,    // Speed of the enter/exit transition.
-        transition: true,   // Set a transition on enter/exit.
-        disableAxis: null,   // What axis should be disabled. Can be X or Y.
-        reset: true,   // If the tilt effect has to be reset on exit.
-        glare: false,  // Enables glare effect
-        maxGlare: 1
-    })
+    // $('.logo-container').tilt({
+    //     maxTilt: 60,
+    //     perspective: 1000,
+    //     easing: "cubic-bezier(.03,.98,.52,.99)",
+    //     scale: 1, 
+    //     speed: 300, 
+    //     transition: true,
+    //     disableAxis: null,.
+    //     reset: true,
+    //     glare: false,
+    //     maxGlare: 1
+    // })
 });
 /*const second = 1000,
     minute = second * 60,
